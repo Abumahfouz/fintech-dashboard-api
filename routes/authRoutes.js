725ce3e99334f3c4a8679445c5f506ed35000eb3
Abumahfouz <mahfoozabubakar@rocketmail.com>
2026-02-02 15:12:56 +0100

    modified login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,41 @@ const express = require('express');
 const router = express.Router();
 const {register, login} = require('../controllers/authController');
 // Registration route
+
+/**
+ * @swagger
+ */
 router.post('/signup', register);
 
 // Login route
+
+/** 
+* @swagger 
+* /auth/login:         
+*   post: 
+*     summary: Login a user and receive JWT 
+*     tags: [Auth] 
+*     requestBody: 
+*       required: true 
+*       content: 
+*         application/json: 
+*           schema: 
+ *             type: object 
+ *             required: 
+ *               - username 
+ *               - password 
+ *             properties: 
+ *               username: 
+ *                 type: string 
+ *               password: 
+ *                 type: string 
+ *     responses: 
+ *       200: 
+ *         description: Login successful 
+ *       401: 
+ *         description: Invalid credentials 
+ */ 
+
 router.post('/login', login);
 
 module.exports = router;
